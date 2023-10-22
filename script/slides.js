aef6d84c0ecf7f13acf747ca7484fb7c8a3307a7
Nikola Pavlov <nikola.pavlov01@gmail.com>
2023-10-22 19:00:27 +0200

    GroundUP v2 (responsive)

---
+++ script/slides.js
@@ -0,0 +1,18 @@
+// function initSlider() {
+
+//     const slidesContainer = document.getElementById("slides-container");
+//     const slide = document.querySelector(".slide");
+//     const prevButton = document.getElementById("slide-arrow-prev");
+//     const nextButton = document.getElementById("slide-arrow-next");
+//     nextButton.addEventListener("click", () => {
+//         console.log("testnext");
+//         const slideWidth = slide.clientWidth;
+//         slidesContainer.scrollLeft += slideWidth;
+//     });
+//     prevButton.addEventListener("click", () => {
+//         console.log("testprev");
+//         const slideWidth = slide.clientWidth;
+//         slidesContainer.scrollLeft -= slideWidth;
+//     });
+//     console.log("test");
+// }
